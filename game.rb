e490d60969472bda358f4cd894058979dc62760e
Zobee <zohebboga@hotmail.com>
2021-06-21 15:56:08 +0000

    moved Question to the Game initializer, and moved duplicate code in the gameloop into its own promptPlayer method

--- game.rb
+++ game.rb
@@ -2,29 +2,32 @@ class Game
   def initialize
     @curr_turn = 1
     @winner = nil
+    @question = Question.new
   end
 
   def assignWinner(winner)
     @winner = winner
   end
 
-  def startGame(player1, player2, question)
+  def promptPlayer(player)
+    puts "#{player.name}'s turn:"
+    newQ = @question.generateQuestion
+    player.answer(newQ)
+  end
+
+  def startGame(player1, player2)
     puts "GAME START"
     winner = nil
     #Game loop
     while player1.lives > 0 && player2.lives > 0
       if @curr_turn == 1
-        puts "#{player1.name}'s turn:"
-        newQ = question.generateQuestion
-        player1.answer(newQ)
+        self.promptPlayer(player1)
         if(player1.lives == 0)
           self.assignWinner(player2)
         end
         @curr_turn = 2
       elsif @curr_turn == 2
-        puts "#{player2.name}'s turn:"
-        newQ = question.generateQuestion
-        player2.answer(newQ)
+        self.promptPlayer(player2)
         if(player2.lives == 0)
           self.assignWinner(player1)
         end
--- main.rb
+++ main.rb
@@ -6,13 +6,5 @@ require "./player.rb"
 player1 = Player.new("Player 1")
 player2 = Player.new("Player 2")
 
-question = Question.new
-# ans = question.generateQuestion
-# player1.answer(ans)
-# puts "Player 1 Lives:"
-# puts player1.lives
-# puts "Player 1 Score:"
-# puts player1.score
-
 game = Game.new
-game.startGame(player1, player2, question)
+game.startGame(player1, player2)
