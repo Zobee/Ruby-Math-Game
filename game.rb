b122c7856039cff66aa4544893ae9a92e4085a10
Zobee <zohebboga@hotmail.com>
2021-06-21 15:36:37 +0000

    Updated Game class with an assignWinner method, and updated winning message to print out the winner's name and score

--- game.rb
+++ game.rb
@@ -1,6 +1,11 @@
 class Game
   def initialize
     @curr_turn = 1
+    @winner = nil
+  end
+
+  def assignWinner(winner)
+    @winner = winner
   end
 
   def startGame(player1, player2, question)
@@ -11,17 +16,19 @@ class Game
       if @curr_turn == 1
         puts "#{player1.name}'s turn:"
         player1.answer(15)
-        puts("Lives Remaining:")
-        puts(player1.lives)
+        if(player1.lives == 0)
+          self.assignWinner(player2)
+        end
         @curr_turn = 2
       elsif @curr_turn == 2
         puts "#{player2.name}'s turn:"
         player2.answer(15)
-        puts("Lives Remaining:")
-        puts(player2.lives)
+        if(player2.lives == 0)
+          self.assignWinner(player1)
+        end
         @curr_turn = 1
       end
     end
-    puts "GAME END #{winner} wins"
+    puts "GAME END #{@winner.name} wins with a score of #{@winner.score}"
   end
 end
--- player.rb
+++ player.rb
@@ -5,16 +5,21 @@ class Player
     @lives = 3
     @score = 0
   end
+  def printScoreLives
+    puts "Score: #{score} Lives: #{lives}"
+  end
   def answer(expectedAnswer)
     puts expectedAnswer
     puts "What will happen??"
     userAnswer = gets.chomp.to_i
     if userAnswer == expectedAnswer
       puts "CORRECT!"
-      @score = @score.to_i + 1
+      @score = @score + 1
+      self.printScoreLives
     else 
       puts "WRONG!"
-      @lives = @lives.to_i - 1
+      @lives = @lives - 1
+      self.printScoreLives
     end
   end
 end
